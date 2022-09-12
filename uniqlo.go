f6a6affd134614fdb0fcd5cd69ebf864d7cc8e36
zzzang12 <sud0@kakao.com>
2022-09-12 23:40:08 +0900

    Trying: 이미지 가져오는 부분에 goroutine 도입 중

--- uniqlo.go
+++ uniqlo.go
@@ -1,16 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 )
 
 func main() {
+	fmt.Println("해보자")
+	//test1()
+	test2()
+}
+
+//func test1() {
+//	listPage := "https://store-kr.uniqlo.com/display/displayShop.lecs?displayNo=NQ1A01A11A02"
+//
+//	resp := httpGet(listPage)
+//	defer resp.Body.Close()
+//
+//	doc, err := goquery.NewDocumentFromReader(resp.Body)
+//	checkError(err)
+//
+//	doc.Find("#content1 .blkMultibuyContent").Each(func(_ int, topic *goquery.Selection) {
+//		topicName := topic.Find("p").Text()
+//		createDirectory(topicName)
+//		topic.Next().Find(".uniqlo_info .item").Each(func(_ int, item *goquery.Selection) {
+//			goodsCode, _ := item.Find(".tumb_img>a").Attr("href")
+//			goodsCode = strings.FieldsFunc(goodsCode, split)[1]
+//			imageAddress, _ := item.Find(".tumb_img>a>img").Attr("src")
+//			imageAddress = strings.Replace(imageAddress, "276", "1000", 1)
+//
+//			createFile(imageAddress, topicName, goodsCode)
+//		})
+//	})
+//}
+
+func test1() {
 	listPage := "https://store-kr.uniqlo.com/display/displayShop.lecs?displayNo=NQ1A01A11A02"
+
+	topicChan := make(chan string, 100)
+	//itemChan := make(chan string)
 
 	resp := httpGet(listPage)
 	defer resp.Body.Close()
@@ -18,18 +52,66 @@ func main() {
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	checkError(err)
 
-	doc.Find("#content1 .blkMultibuyContent").Each(func(_ int, topic *goquery.Selection) {
-		topicName := topic.Find("p").Text()
-		createDirectory(topicName)
-		topic.Next().Find(".uniqlo_info .item").Each(func(_ int, item *goquery.Selection) {
-			goodsCode, _ := item.Find(".tumb_img>a").Attr("href")
-			goodsCode = strings.FieldsFunc(goodsCode, split)[1]
-			imageAddress, _ := item.Find(".tumb_img>a>img").Attr("src")
-			imageAddress = strings.Replace(imageAddress, "276", "1000", 1)
-
-			createFile(imageAddress, topicName, goodsCode)
-		})
+	topicWg := &sync.WaitGroup{}
+	sel := doc.Find("#content1 .blkMultibuyContent")
+	topicNumber := getTopicNumber(sel)
+	topicWg.Add(topicNumber)
+	defer topicWg.Wait()
+	sel.Each(func(_ int, topic *goquery.Selection) {
+		go getTopic(topic, topicChan, topicWg)
 	})
+
+	//for i := 0; i < topicNumber; i++ {
+	//	fmt.Println(<-topicChan)
+	//}
+}
+
+func test2() {
+	listPage := "https://store-kr.uniqlo.com/display/displayShop.lecs?displayNo=NQ1A01A11A02"
+
+	topicChan := make(chan string, 100)
+	//itemChan := make(chan string)
+
+	resp := httpGet(listPage)
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	checkError(err)
+
+	topicWg := &sync.WaitGroup{}
+	sel := doc.Find("#content1 .blkMultibuyContent")
+	topicNumber := getTopicNumber(sel)
+	topicWg.Add(topicNumber)
+	defer topicWg.Wait()
+	sel.Each(func(_ int, topic *goquery.Selection) {
+		go getTopic(topic, topicChan, topicWg)
+	})
+
+	//for i := 0; i < topicNumber; i++ {
+	//	fmt.Println(<-topicChan)
+	//}
+}
+
+func getTopic(topic *goquery.Selection, topicChan chan string, topicWg *sync.WaitGroup) {
+	topicName := topic.Find("p").Text()
+	topicChan <- topicName
+	createDirectory(topicName)
+	topic.Next().Find(".uniqlo_info .item").Each(func(_ int, item *goquery.Selection) {
+		goodsCode, _ := item.Find(".tumb_img>a").Attr("href")
+		goodsCode = strings.FieldsFunc(goodsCode, split)[1]
+		imageAddress, _ := item.Find(".tumb_img>a>img").Attr("src")
+		imageAddress = strings.Replace(imageAddress, "276", "1000", 1)
+
+		createFile(imageAddress, topicName, goodsCode)
+	})
+	topicWg.Done()
+}
+
+func getTopicNumber(sel *goquery.Selection) (topicNumber int) {
+	sel.Each(func(_ int, _ *goquery.Selection) {
+		topicNumber++
+	})
+	return
 }
 
 func httpGet(url string) *http.Response {
