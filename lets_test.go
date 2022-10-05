b4f2ae05ea99f7d8bfb4921e048a5671882660b7
zzzang12 <sud0@kakao.com>
2022-10-05 11:55:12 +0900

    Test: 벤치마크 코드 수정

--- lets_test.go
+++ lets_test.go
@@ -1,36 +1,11 @@
 package main
 
 import (
-	"github.com/PuerkitoBio/goquery"
 	"testing"
 )
 
-func prerequisite() (doc *goquery.Document) {
-	listPage := "https://store-kr.uniqlo.com/display/displayShop.lecs?displayNo=NQ1A01A11A02"
-	resp := httpGet(listPage)
-	defer resp.Body.Close()
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	checkError(err)
-	return
-}
-
-func BenchmarkTest1(b *testing.B) {
-	doc := prerequisite()
+func BenchmarkTest(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		test1(doc)
-	}
-}
-
-func BenchmarkTest2(b *testing.B) {
-	doc := prerequisite()
-	for i := 0; i < b.N; i++ {
-		test2(doc)
-	}
-}
-
-func BenchmarkTest3(b *testing.B) {
-	doc := prerequisite()
-	for i := 0; i < b.N; i++ {
-		test3(doc)
+		main()
 	}
 }
